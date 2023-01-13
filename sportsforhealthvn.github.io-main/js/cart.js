4c44e66b1ae47644fa0710f03d273f6a20eef2bc
phamquang1203 <101632202+phamquang1203@users.noreply.github.com>
2023-01-13 18:59:19 +0700

    Add files via upload

---
+++ sportsforhealthvn.github.io-main/js/cart.js
@@ -0,0 +1,159 @@
+let label = document.getElementById("label");
+let ShoppingCart = document.getElementById("shopping-cart");
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+
+let calculation = () => {
+  let cartIcon = document.getElementById("cartAmount");
+  cartIcon.innerHTML = basket.map((x) => x.item).reduce((x, y) => x + y, 0);
+};
+
+calculation();
+
+let generateCartItems = () => {
+  if (basket.length !== 0) {
+    return (ShoppingCart.innerHTML = basket
+      .map((x) => {
+        let { id, item } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+        return `
+      <div class="cart-item">
+        <img src=${search.img} alt="" />
+        <div class="details">
+
+          <div class="title-price-x">
+              <h4 class="title-price">
+                <p>${search.name}</p>
+                <p class="cart-item-price">${search.price}VND</p>
+              </h4>
+              <i onclick="removeItem(${id})" class="bi bi-x-lg"></i>
+          </div>
+
+          <div class="buttons">
+              <i onclick="decrement(${id})" class="bi bi-dash-lg"></i>
+              <div id=${id} class="quantity">${item}</div>
+              <i onclick="increment(${id})" class="bi bi-plus-lg"></i>
+          </div>
+
+          <h3>${item * search.price}VND</h3>
+        </div>
+      </div>
+      `;
+      })
+      .join(""));
+  } else {
+    ShoppingCart.innerHTML = ``;
+    label.innerHTML = `
+    <h2>Cart is Empty</h2>
+    <a href="shop.html">
+      <button class="ShopBtn">Back to shop</button>
+    </a>
+    `;
+  }
+};
+
+generateCartItems();
+
+let increment = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) {
+    let shopItem = shopItemsData.find((x) => x.id === selectedItem.id);
+    console.log(shopItem);
+    basket.push({
+      id: selectedItem.id,
+      item: 1,
+      name: shopItem.name,
+      price: shopItem.price,
+    });
+  } else {
+    search.item += 1;
+  }
+
+  generateCartItems();
+  update(selectedItem.id);
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+let decrement = (id) => {
+  let selectedItem = id;
+  let search = basket.find((x) => x.id === selectedItem.id);
+
+  if (search === undefined) return;
+  else if (search.item === 0) return;
+  else {
+    search.item -= 1;
+  }
+  update(selectedItem.id);
+  basket = basket.filter((x) => x.item !== 0);
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let update = (id) => {
+  let search = basket.find((x) => x.id === id);
+  // console.log(search.item);
+  document.getElementById(id).innerHTML = search.item;
+  calculation();
+  TotalAmount();
+};
+
+let removeItem = (id) => {
+  let selectedItem = id;
+  // console.log(selectedItem.id);
+  basket = basket.filter((x) => x.id !== selectedItem.id);
+  generateCartItems();
+  TotalAmount();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let clearCart = () => {
+  basket = [];
+  generateCartItems();
+  localStorage.setItem("data", JSON.stringify(basket));
+};
+
+let TotalAmount = () => {
+  if (basket.length !== 0) {
+    let amount = basket
+      .map((x) => {
+        let { item, id } = x;
+        let search = shopItemsData.find((y) => y.id === id) || [];
+
+        return item * search.price;
+      })
+      .reduce((x, y) => x + y, 0);
+    // console.log(amount);
+    label.innerHTML = `
+    
+    <div class="container" id="totalprice">
+    <h2>Total Bill : ${amount} VND</h2>
+    <button onclick="checkOut()" class="checkout">Checkout</button>
+    <button onclick="clearCart()" class="removeAll">Clear Cart</button>
+</div>
+    `;
+  } else return;
+};
+
+TotalAmount();
+
+// program to generate random strings
+
+// declare all characters
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function generateString(length) {
+  let result = " ";
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(
+      Math.floor(Math.random() * charactersLength)
+    ).toUpperCase;
+  }
+
+  return result;
+}
+
+let checkOut = () => {
+  location.replace("./checkout-form.html");
+};
---
+++ sportsforhealthvn.github.io-main/js/script-checkout-form.js
@@ -0,0 +1,131 @@
+let checkoutForm = document.getElementById("checkout-form");
+let basket = JSON.parse(localStorage.getItem("data")) || [];
+let format = '<p><a href="#">Name</a> <span class="price">Price</span></p>';
+let items = "";
+let total = 0;
+let amount = 0;
+for (let i = 0; i < basket.length; i++) {
+  items += format
+    .replace("Name", basket[i].name)
+    .replace("Price", basket[i].price);
+  console.log(basket[i]);
+}
+for (let i = 0; i < basket.length; i++) {
+  total += parseFloat(basket[i].price);
+  amount++;
+  console.log(basket[i]);
+}
+console.log(items);
+let generateCheckoutForm = () => {
+  if (basket.length !== 0)
+    return (checkoutForm.innerHTML = `
+      <div class="container" style="margin-top:30px">
+      <div class="row">
+        <div class="col-75">
+          <div class="container" style="background-color: #f2f2f2;
+          padding: 5px 20px 15px 20px;
+          border: 1px solid lightgrey;
+          border-radius: 3px;
+          margin-bottom: 30px">
+              <div class="row">
+                <div class="col-50">
+                  <h3>Billing Address</h3>
+                  <label for="fname"
+                    ><i class="fa fa-user"></i> Tên đầy đủ</label
+                  >
+                  <input
+                    type="text"
+                    id="fname"
+                    name="firstname"
+                  />
+                  <label for="email"
+                    ><i class="fa fa-envelope"></i> Email</label
+                  >
+                  <input
+                    type="text"
+                    id="email"
+                    name="email"
+                  />
+                  <label for="adr"
+                    ><i class="fa fa-address-card-o"></i> Địa chỉ</label
+                  >
+                  <input
+                    type="text"
+                    id="adr"
+                    name="address"
+                  />
+                  <label for="city"
+                    ><i class="fa fa-institution"></i> Thành phố</label
+                  >
+                  <input
+                    type="text"
+                    id="city"
+                    name="city"
+                  />
+
+                </div>
+              </div>
+              <label>
+                <input type="checkbox" checked="checked" name="sameadr" />
+                Shipping address same as billing
+              </label>
+              <input id="bought" type="submit" onclick="bought()" value="Mua hàng" class="btn" />
+          </div>
+        </div>
+        <div class="col-25">
+          <div class="container" style="background-color: #f2f2f2;
+          padding: 5px 20px 15px 20px;
+          border: 1px solid lightgrey;
+          border-radius: 3px;">
+            <h4>
+              Cart
+              <span class="price" style="color: black"
+                ><i class="fa fa-shopping-cart"></i> <b>` +
+                amount +
+                `</b></span
+              >
+            </h4>
+            ` +
+      items +
+      `
+            <hr />
+            <p>
+              Total <span class="price" style="color: black"><b>` +
+              total +
+              `</b></span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>`);
+};
+
+generateCheckoutForm();
+
+function bought()
+{
+    document.getElementById('bought').value = "Sending email..."
+    document.getElementById('bought').disabled = true
+    $.ajax
+    ({
+        url: 'https://77cjeb.deta.dev/',
+        type: 'POST',
+        contentType: 'application/json',
+        data: JSON.stringify
+        ({
+            name: document.getElementById('fname').value,
+            email:document.getElementById('email').value,
+            address:document.getElementById('adr').value,
+            city:document.getElementById('city').value,
+            items:basket
+        }),
+        success: function(data)
+        {
+            window.location.href = "email_success.html";
+        },
+        error: function()
+        {
+            window.location.href = "email_fail.html";
+        },
+    });
+}
